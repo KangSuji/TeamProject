4ed3c69e3aed9df74f37f106c7bd180e6a5ac792
KangSuji <67314565+KangSuji@users.noreply.github.com>
2021-01-02 17:38:13 +0900

    test folder

---
+++ Spboot_AnimalGoodsShop/src/test/java/com/spboot/shop/SpbootAnimalGoodsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spboot.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootAnimalGoodsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
